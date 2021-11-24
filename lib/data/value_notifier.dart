5cc21e019a5e3f0f3c6c5da50590e03f12e9c927
wq <wq@qq.com>
2021-11-24 11:40:53 +0800

    '增加一个支持不发布通知的数据监听器'

--- lib/data/value_notifier.dart
+++ lib/data/value_notifier.dart
@@ -10,16 +10,24 @@ class NoNotifyValueNotifier<T> extends ChangeNotifier
   T _value;
 
   set value(T newValue) {
-    if (_value == newValue) return;
+    if (_value == newValue && !_needNotify) {
+      //这里需要在  _needNotify = true  时放行
+      //可以避免在重复设置值 a ，但是却需要更新时，各个观察者无法收到通知的问题
+      return;
+    }
     _value = newValue;
     notifyListeners();
   }
+
+  ///是否需要更新了
+  bool _needNotify = false;
 
   ///更新值，但是不通知更新
   void setValueWithoutNotify(T newValue) {
     if (_value == newValue) return;
     _value = newValue;
     //do not notify
+    _needNotify = true;
   }
 
   @override
