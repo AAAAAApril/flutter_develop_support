f286a1f996056d1b3e5ce6b66bd1ec6ca240e47f
wq <wq@qq.com>
2022-04-27 10:31:31 +0800

    增加一个日志打印工具函数

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
@@ -36,4 +38,52 @@ Future<String?> pasteTextFromClipboard() {
   return Clipboard.getData('text/plain').then<String?>(
     (value) => value?.text,
   );
+}
+
+///长数据量时分段打印
+void longPrint(
+  Object? source, {
+  String? tag,
+  int limitLength = 500,
+  bool printWhenDebugOnly = true,
+}) {
+  if (printWhenDebugOnly && !isDebug) {
+    return;
+  }
+  if (tag != null) {
+    _flutterPrint(tag);
+  }
+  if (source == null) {
+    return;
+  }
+  String target;
+  if (source is Map<String, dynamic>) {
+    target = jsonEncode(source);
+  } else {
+    target = source.toString();
+  }
+  if (target.isEmpty) {
+    return;
+  }
+  if (target.length < limitLength) {
+    _flutterPrint(target);
+  } else {
+    var outStr = StringBuffer();
+    for (var index = 0; index < target.length; index++) {
+      outStr.write(target[index]);
+      if (index % limitLength == 0 && index != 0) {
+        _flutterPrint(outStr.toString());
+        outStr.clear();
+        var lastIndex = index + 1;
+        if (target.length - lastIndex < limitLength) {
+          _flutterPrint(target.substring(lastIndex, target.length));
+          break;
+        }
+      }
+    }
+  }
+}
+
+void _flutterPrint(String value) {
+  print(value);
 }
