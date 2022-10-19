b75ebcc7e3679ad2f576fb9a80aab729285b6208
wq <wq@qq.com>
2022-10-19 14:38:32 +0800

    标记当前运行模式的常量改为使用系统提供的值

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,17 +1,24 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 ///是否是 debug 状态
-final bool isDebug = () {
-  bool result = false;
-  assert(() {
-    result = true;
-    return true;
-  }());
-  return result;
-}();
+@Deprecated(
+  'use kDebugMode instead,'
+  'which needs import package:flutter/foundation.dart',
+)
+const bool isDebug = kDebugMode;
+
+///当前是否在 Debug 状态
+const bool inDebug = kDebugMode;
+
+///当前是否在 Release 状态
+const bool inRelease = kReleaseMode;
+
+///当前是否在 Profile 状态
+const bool inProfile = kProfileMode;
 
 ///显示输入法
 Future<void> showInputMethod() {
@@ -47,7 +54,7 @@ void longPrint({
   int limitLength = 500,
   bool printWhenDebugOnly = true,
 }) {
-  if (printWhenDebugOnly && !isDebug) {
+  if (printWhenDebugOnly && !kDebugMode) {
     return;
   }
   if (tag != null) {
