de5716728083bc7700bbc41721671729cc595b70
April <569551020@qq.com>
2024-03-03 20:32:18 +0800

    移除监听键盘事件的工具，添加一个输入内容监听器

---
+++ lib/src/utils/text_input_receiver.dart
@@ -0,0 +1,161 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+///在桌面端，没有输入框时，接收来自键盘输入的内容
+class TextInputReceiver {
+  static FocusNode createFocusNode() => TextFieldFocusNode();
+
+  static void addListener(bool Function(String inputText) listener) {
+    _InputClient.listeners.add(listener);
+  }
+
+  static void removeListener(bool Function(String inputText) listener) {
+    _InputClient.listeners.remove(listener);
+  }
+
+  static void start() {
+    TextInputReceiver._instance._start();
+  }
+
+  static void stop() {
+    TextInputReceiver._instance._stop();
+  }
+
+  static void clearCache() {
+    TextInputReceiver._instance._clearCache();
+  }
+
+  static final TextInputReceiver _instance = TextInputReceiver._();
+
+  TextInputReceiver._();
+
+  ///是否允许接收
+  bool _enable = false;
+
+  ///编辑框是否有焦点
+  bool _fieldFocused = false;
+
+  TextInputConnection? _inputConnection;
+
+  bool get _hasConnection => _inputConnection != null;
+
+  ///开始接收
+  void _start() {
+    _enable = true;
+    _clearCache();
+    _onFocusNodeChanged();
+  }
+
+  ///停止接收
+  void _stop() {
+    _enable = false;
+    _clearCache();
+    _onFocusNodeChanged();
+  }
+
+  ///清除输入缓存
+  void _clearCache() {
+    _InputClient.instance.clearValue();
+  }
+
+  Timer? _delayTimer;
+
+  void _onFocusNodeChanged() {
+    _delayTimer?.cancel();
+    _delayTimer = Timer(Duration.zero, _delayed);
+  }
+
+  void _delayed() {
+    _fieldFocused = TextFieldFocusNode._fieldsNodes.any(
+      (element) => element.hasPrimaryFocus,
+    );
+    if (!_fieldFocused && _enable) {
+      if (!_hasConnection) {
+        _inputConnection = TextInput.attach(
+          _InputClient.instance,
+          const TextInputConfiguration(inputAction: TextInputAction.none),
+        );
+        _inputConnection?.show();
+      }
+    } else {
+      _inputConnection?.close();
+      _inputConnection = null;
+    }
+  }
+}
+
+class TextFieldFocusNode extends FocusNode {
+  static final Set<FocusNode> _fieldsNodes = <FocusNode>{};
+
+  TextFieldFocusNode({
+    super.debugLabel,
+    super.canRequestFocus,
+    super.descendantsAreFocusable,
+    super.descendantsAreTraversable,
+    super.onKeyEvent,
+    super.skipTraversal,
+  }) {
+    TextFieldFocusNode._fieldsNodes.add(this);
+    addListener(TextInputReceiver._instance._onFocusNodeChanged);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    TextFieldFocusNode._fieldsNodes.remove(this);
+  }
+}
+
+class _InputClient with TextInputClient {
+  static final _InputClient instance = _InputClient._();
+  static final Set<bool Function(String text)> listeners = {};
+
+  _InputClient._();
+
+  TextEditingValue _editingValue = TextEditingValue.empty;
+
+  ///清空输入内容
+  void clearValue() {
+    _editingValue = TextEditingValue.empty;
+  }
+
+  @override
+  void connectionClosed() {
+    clearValue();
+    TextInputReceiver._instance._inputConnection?.connectionClosedReceived();
+    TextInputReceiver._instance._inputConnection = null;
+  }
+
+  @override
+  AutofillScope? get currentAutofillScope => null;
+
+  @override
+  TextEditingValue get currentTextEditingValue => _editingValue;
+
+  @override
+  void performAction(TextInputAction action) {}
+
+  @override
+  void performPrivateCommand(String action, Map<String, dynamic> data) {}
+
+  @override
+  void showAutocorrectionPromptRect(int start, int end) {}
+
+  @override
+  void updateEditingValue(TextEditingValue value) {
+    _editingValue = value;
+    if (value.composing.isValid) {
+      return;
+    }
+    for (var element in List.of(listeners).reversed) {
+      if (element.call(value.text)) {
+        return;
+      }
+    }
+  }
+
+  @override
+  void updateFloatingCursor(RawFloatingCursorPoint point) {}
+}
